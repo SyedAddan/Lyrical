1512bc01e57c74d29ba52c4870a9f8c6a587b292
syedaddan <syedaddan@gmail.com>
2023-06-01 20:15:53 +0500

    other py files structured

--- src/data/make_processed.py
+++ src/data/make_processed.py
@@ -2,26 +2,24 @@
 import os
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 
 
 def makeProcessed():
     """ 
-        Runs data processing scripts to turn raw data from (/projectdir/data/raw) into
-        cleaned data ready to be analyzed (saved in /projectdir/data/processed).
+        Builds Features from processed data in /projectdir/data/processed into
+        cleaned data ready to be analyzed (saved in /projectdir/data/features).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('building features from processed data')
     
-    input_filepath = Path("../../data/raw")
+    input_filepath = Path("../../data/processed")
+    
+    logger.info('input_filepath: {}'.format(input_filepath))
+    logger.info("features built")
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     makeProcessed()
