31a49e10197fb21edf80392b7d8da458eeea1912
syedaddan <syedaddan@gmail.com>
2023-06-04 20:25:21 +0500

    added preprocessing to the raw data but is long

--- src/data/make_processed.py
+++ src/data/make_processed.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
 import logging
+import pandas as pd
+import numpy as np
+import gc
 from pathlib import Path
 
 
@@ -15,7 +18,54 @@ def makeProcessed():
     input_filepath = Path("../../data/processed")
     
     logger.info('input_filepath: {}'.format(input_filepath))
-    logger.info("features built")
+    
+    # this can take more than 12 hours to run
+    with pd.read_csv(
+        "../data/raw/song_lyrics.csv",
+        chunksize=5 * 10**3,
+        usecols=["title", "artist", "year", "tag", "views", "lyrics"],
+        dtype={"year": np.int16, "views": np.int32}
+    ) as chunks:
+
+        for idx, chunk in enumerate(chunks):
+            print(f"Processing chunk {idx}")
+
+            # drop N.A. lyrics
+            chunk = chunk.dropna(subset=["lyrics"])
+            
+            # drop romanizations
+            chunk = chunk[chunk["artist"] != "Genius Romanizations"]
+            chunk = chunk[~chunk["title"].str.contains(r"\(?romanized\)?", regex=True, na=False, case=False)]
+
+            # remove invalid years
+            chunk = chunk[chunk["year"] < 2023]
+            
+            # remove duplicated entries
+            chunk = chunk.drop_duplicates(subset=["title", "artist", "year"])
+            
+            # remove special characters from lyrics
+            pattern = r"(?m)^\[.*?\]$"
+            chunk["lyrics"] = chunk["lyrics"].str.replace(pattern, "", regex=True)
+            
+            # remove empty lines
+            pattern = r"\n|\n\n"
+            chunk["lyrics"] = chunk["lyrics"].str.replace(pattern, " ", regex=True)
+
+            # drop lyrics that are too short or too long
+            chunk = chunk[chunk["lyrics"].str.len().between(10**2, 10**5)]
+
+            # analyze language
+            chunk["language"] = chunk["lyrics"].apply(identify_language)
+            print(f'{len(chunk[chunk["language_cld3"].isna()])} not identified lyrics using by the language detection model.')
+            
+            # drop non-english lyrics
+            chuck = chunk[chunk["language"] == "en"][["artist", "tag", "lyrics"]]
+            
+            # save processed data
+            chunk.to_csv("../data/processed/lyrics_processed.csv", mode="a", header=not os.path.exists("../data/processed/lyrics_processed.csv"), index=False)
+            
+            del chunk
+            gc.collect()
 
 
 if __name__ == '__main__':
--- src/data/make_raw.py
+++ src/data/make_raw.py
@@ -10,17 +10,17 @@ PROJECT_DIR = Path(__file__).resolve().parents[2]
 
 def makeRaw():
     """ 
-        Fetches the dataset used from the Kaggle dataset: 'nikhilnayak123/5-million-song-lyrics-dataset'
+        Fetches the dataset used from the Kaggle notebook's output: 'carlosgdcj/song-lyrics'
         and places it into (/projectdir/data/raw).
     """
     logger = logging.getLogger(__name__)
     logger.info('fetching raw data from kaggle')
     
-    dataset_url = "nikhilnayak123/5-million-song-lyrics-dataset"
+    dataset_url = "carlosgdcj/song-lyrics"
     destination_path = os.path.join(PROJECT_DIR, "data", "raw")
     if not(os.path.exists(destination_path)):
         os.makedirs(destination_path)
-    destination_zip_path = os.path.join(destination_path, "5-million-song-lyrics-dataset.zip")
+    destination_zip_path = os.path.join(destination_path, "df_eng.zip")
     destination_csv_path = os.path.join(destination_path, "song_lyrics.csv")
     
     # check if raw csv file already exists
@@ -28,14 +28,14 @@ def makeRaw():
         # if it doesn't, check if the raw zip file exists
         if not(os.path.exists(destination_zip_path)):
             # if it doesn't, fetch it from kaggle
-            command = f"kaggle datasets download -d {dataset_url}"
+            command = f"kaggle kernels output {dataset_url}"
             
             # download the raw zip file
             try:
                 subprocess.call(command, shell=True)
                 logger.info(f'raw zip file fetched from {dataset_url}')
                 logger.info(f"now placing raw zip file into {destination_path}")
-                os.rename("5-million-song-lyrics-dataset.zip", destination_zip_path)
+                os.rename("df_eng.zip", destination_zip_path)
                 logger.info(f'raw zip file placed into {destination_path}')
                 
             except:
@@ -52,7 +52,7 @@ def makeRaw():
             with zipfile.ZipFile(destination_zip_path, 'r') as zip_ref:
                 zip_ref.extractall(destination_path)
             os.remove(destination_zip_path)
-            os.rename(os.path.join(destination_path, "ds2.csv"), destination_csv_path)
+            os.rename(os.path.join(destination_path, "df_eng.csv"), destination_csv_path)
             logger.info(f'raw zip file unzipped to {destination_path}')
         except:
             logger.error(f'failed to unzip raw zip file to {destination_path}')
